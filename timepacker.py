8ce885b5339fb9d9fd6781bc6ac563961b4c1f5d
Micah R Ledbetter <mrled@younix.us>
2013-06-24 01:06:50 -0500

    Added timepacker code from zk/structure.py (pristine)

---
+++ timepacker.py
@@ -0,0 +1,53 @@
+import json
+import datetime
+
+time_fmt_Z      = '%Y-%m-%dT%H:%M:%SZ'  # The format with Z at the end, signifying UTC
+time_fmt_offset = '%Y-%m-%dT%H:%M:%S%z' # The format with +/-HHMM at the end, signifying offset.
+
+def unpack_timestamp(timestamp):
+    """
+    Unpack a string object that conforms to my timestamp specification into a datetime.datetime object.
+    """
+    if type(timestamp) is not str:
+        raise Exception("ERROR: Passed an object to unpack_timestamp() that was not a string.")
+
+    
+    try:
+        # try the format with a Z at the end signifying UTC:
+        ret = datetime.datetime.strptime(timestamp, time_fmt_Z)
+        # if that works, it'll be valid, but without a .tzinfo attribute, and therefore it will be what python calls a "naive" object - unaware of the timezone. 
+        # munge it to include an offset of 00:00, and then reparse with strptime; now we have a .tzinfo attribute, perfect
+        new_timestamp = timestamp[0:-1] + "+0000"
+        ret = datetime.datetime.strptime(new_timestamp, time_fmt_offset)
+        return ret
+    except ValueError as ve:
+        pass
+    
+    try:
+        # try the format with an offset of the format +/-HHMM (which is easy to do in python because of %z)
+        # this ends up timezonified also
+        ret = datetime.datetime.strptime(timestamp, time_fmt_offset)
+        return ret
+    except ValueError as ve:
+        pass
+
+    try:
+        # now try the same thing with a colon: +/-HH:MM (which is still correct rfc3339, but I have to munge the timestamp so that %z will take it)
+        # TODO: This is experimental or whatever. doc/time-stuff.markdown says this is unsupported. Should either remove this or
+        #       fully support the rest of rfc3339 for consistency.
+        new_zoneinfo = timestamp[-6:-3] + timestamp[-2:]
+        new_timestamp = timestamp[0:-6] + new_zoneinfo
+        ret = datetime.datetime.strptime(new_timestamp, time_fmt_offset)
+        return ret
+    except ValueError as ve:
+        pass
+
+    raise Exception("Could not understand timestamp format.")
+
+def pack_timestamp(dt):
+    """
+    Pack a datetime object dt into a timestamp formatted for use with zk
+    """
+    if type(dt) is not datetime.datetime and type(dt) is not datetime.date:
+        raise Exception("ERROR: Passed an object to pack_timestamp() that was not a datetime.datetime or a datetime.date.")
+    return dt.strftime('%Y-%m-%dT%H:%M:%S%z')
